bca1a503c4cbeb58aa12b8c9628ed8e53dadefbf
Nityanand Kumar <nityanandkumarm@gmail.com>
2024-11-26 08:04:28 +0530

    Adding System Design Problems and Codes to the Repo

---
+++ tool/src/main/java/com/pricing/tool/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.pricing.tool.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**");
+    }
+}
